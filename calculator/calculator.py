1a137519df9f65ea9aca180b5b7ad16317b51d64
krishna shrimali <krishnashrimali008@gmail.com>
2025-08-28 12:59:12 +0530

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,81 +1,214 @@
 import tkinter as tk
-
-global num_var,num_add,show,test
-
+global v,num,num1
 num = []
-
-def one():
-        num.append("1")
-
-        
-def two():
-        num.append("2")
-        
-
-def three():
-        num.append("3")
-        
-        
-
-def four():
-        num.append("4")
-      
-
-def five():
-        num.append("5")
+num1 = []
 
 
-def six():
-        num.append("6")
-      
 
-def seven():
-        num.append("7")
-         
+def adder():
+    global num,num1
+    a1 = ''.join(num)
+    a2 = ''.join(num1) 
+    ans = int(a1) + int(a2)
+    num1.clear()
+    result.config(text=str(ans))
+   
+   
 
-def eight():
-        num.append("8")
-       
-def nine():
-        num.append("9")
-        
+def subber():
+    global num,num1
+    a1 = ''.join(num)
+    a2 = ''.join(num1) 
+    ans = int(a1) - int(a2)
+    num1.clear()
+    result.config(text=str(ans))
 
-def equal():
-        result.config(text=''.join(num))
+def multiplier():
+    global num,num1
+    a1 = ''.join(num)
+    a2 = ''.join(num1) 
+    ans = int(a1) * int(a2)
+    num1.clear()
+    result.config(text=str(ans))   
 
-def clear():
-        num.clear()
-        result.config(text=str("0.0"))
+def divisor():
+    global num,num1
+    a1 = ''.join(num)
+    a2 = ''.join(num1) 
+    ans = int(a1) / int(a2)
+    num1.clear()
+    result.config(text=str(ans))   
 
 
 root  = tk.Tk()
 root.resizable(False,False)
-root.geometry("300x500")
+root.geometry("300x550")
 root.title("calculator")
+
+v = tk.IntVar(value=0)
+k = tk.IntVar(value=0)
+
+def radio1():
+    if v.get() == 1:
+        b1.config(state="disabled")
+       
+    elif v.get() == 0 :
+        b1.config(state="active")
+        
+    
+def radio2():
+    if k.get() == 1 :
+        bb.config(state="disabled")
+    elif k.get() == 0 :
+        bb.config(state="active")
+
+def zero():
+    if v.get() == 1:
+        num.append("0")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("0")
+        result.config(text=''.join(num1))
+  
+
+def one():
+    if v.get() == 1:
+        num.append("1")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("1")
+        result.config(text=''.join(num1))
+   
+
+def two():
+    if v.get() == 1:
+        num.append("2")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("2")
+        result.config(text=''.join(num1))
+    
+
+def three():
+    if v.get() == 1:
+        num.append("3")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("3")
+        result.config(text=''.join(num1))
+     
+
+def four():
+    if v.get() == 1:
+        num.append("4")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("4")
+        result.config(text=''.join(num1))
+    
+
+def five():
+    if v.get() == 1:
+        num.append("5")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("5")
+        result.config(text=''.join(num1))
+ 
+
+def six():
+    if v.get() == 1:
+        num.append("6")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("6")
+        result.config(text=''.join(num1))
+          
+
+def seven():
+    if v.get() == 1:
+        num.append("7")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("7")
+        result.config(text=''.join(num1))
+    
+
+def seven():
+    if v.get() == 1:
+        num.append("7")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("7")
+ 
+
+def seven():
+    if v.get() == 1:
+        num.append("7")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("7")
+        result.config(text=''.join(num1))
+ 
+
+def eight():
+    if v.get() == 1:
+        num.append("8")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("8")
+        result.config(text=''.join(num1))
+
+
+def nine():
+    if v.get() == 1:
+        num.append("9")
+        result.config(text=''.join(num))
+    elif k.get() == 1:
+        num1.append("9")
+        result.config(text=''.join(num1))
+ 
+
+def clear():
+    num.clear()
+    num1.clear()
+    result.config(text="0.0")
+
+
 
 
 label1 = tk.Label(text="calculator",font=("calibri",20))
-
-
 result = tk.Label(text="0.0",font=("calibri",18),justify="left",borderwidth=2,relief="solid")
-button1 = tk.Button(text="1",font=("calibri",30),command=one,cursor="hand2")
-button2 = tk.Button(text="2",font=("calibri",30),command=two,cursor="hand2")
-button3 = tk.Button(text="3",font=("calibri",30),command=three,cursor="hand2")
-button4 = tk.Button(text="4",font=("calibri",30),command=four,cursor="hand2")
-button5 = tk.Button(text="5",font=("calibri",30),command=five,cursor="hand2")
-button6 = tk.Button(text="6",font=("calibri",30),command=six,cursor="hand2")
-button7 = tk.Button(text="7",font=("calibri",30),command=seven,cursor="hand2")
-button8 = tk.Button(text="8",font=("calibri",30),command=eight,cursor="hand2")
-button9 = tk.Button(text="9",font=("calibri",30),command=nine,cursor="hand2")
-eq = tk.Button(text="=",font=("calibri",30),command=equal,cursor="hand2")
-cls = tk.Button(text="clr",font=("calibri",30),command=clear,cursor="hand2")
+
+bb = tk.Checkbutton(text="first number",font=("calibri",8),variable=v,onvalue=1,offvalue=0,command=radio1)
+b1 = tk.Checkbutton(text="second number",font=("calibri",8),variable=k,onvalue=1,offvalue=0,command=radio2)
+
+button0 = tk.Button(text="0",font=("calibri",30),cursor="hand2",command=zero)
+button1 = tk.Button(text="1",font=("calibri",30),cursor="hand2",command=one)
+button2 = tk.Button(text="2",font=("calibri",30),cursor="hand2",command=two)
+button3 = tk.Button(text="3",font=("calibri",30),cursor="hand2",command=three)
+button4 = tk.Button(text="4",font=("calibri",30),cursor="hand2",command=four)
+button5 = tk.Button(text="5",font=("calibri",30),cursor="hand2",command=five)
+button6 = tk.Button(text="6",font=("calibri",30),cursor="hand2",command=six)
+button7 = tk.Button(text="7",font=("calibri",30),cursor="hand2",command=seven)
+button8 = tk.Button(text="8",font=("calibri",30),cursor="hand2",command=eight)
+button9 = tk.Button(text="9",font=("calibri",30),cursor="hand2",command=nine)
+
+cls = tk.Button(text="clr",font=("calibri",18),cursor="hand2",command=clear)
+ad = tk.Button(text="add",font=("calibri",18),cursor="hand2",command=adder)
+sub = tk.Button(text="sub",font=("calibri",18),cursor="hand2",command=subber)
+mul = tk.Button(text="mul",font=("calibri",18),cursor="hand2",command=multiplier)
+div = tk.Button(text="div",font=("calibri",18),cursor="hand2",command=divisor)
 
 
 
 
 
-label1.pack(pady=20)
-result.place(x=20,y=60)
+
+bb.place(x = 10,y = 10)
+b1.place(x = 10,y = 30)
+label1.place(x = 110,y  = 5)
+result.place(x=50,y=60)
 button1.place(x = 50,y=100)
 button2.place(x= 110,y = 100)
 button3.place(x = 170,y= 100)
@@ -85,8 +218,12 @@ button6.place(x = 170, y = 190)
 button7.place(x= 50,y=280)
 button8.place(x= 110,y=280)
 button9.place(x= 170,y=280)
-eq.place(x = 50,y = 370)
-cls.place(x = 110,y = 370)
+button0.place(x = 240,y = 190)
+cls.place(x = 50,y = 370)
+ad.place(x = 110,y = 370)
+sub.place(x = 170,y = 370)
+mul.place(x = 50,y = 460)
+div.place(x = 110,y = 460)
 
 
 root.mainloop()
